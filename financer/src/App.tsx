cd148d9c10224bb295788bbb977d4f26afc760aa
egsir1 <egsir111@gmail.com>
2023-12-17 10:15:43 +0900

    feat: react router & navigation

--- financer/src/App.tsx
+++ financer/src/App.tsx
@@ -1,16 +1,25 @@
 import { useMemo } from "react";
-import { createTheme } from "@mui/material";
+import { createTheme, Box } from "@mui/material";
 import { themeSettings } from "./theme";
 import { ThemeProvider, CssBaseline } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "@/scenes/navbar";
 
 function App() {
   const theme = useMemo(() => createTheme(themeSettings), []);
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        hi
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height={"100%"} padding={"1rem 2rem 4rem 2rem"}>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<div>Dashboard page</div>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
